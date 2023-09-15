db857ffd8f3b9c18ec4d687d327e06fa438489d3
JustWaveThings <justwavethings@proton.me>
2023-09-15 15:06:29 -0400

    Add express-rate-limit

--- app.js
+++ app.js
@@ -6,20 +6,31 @@ import logger from 'morgan';
 import compression from 'compression';
 import helmet from 'helmet';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 const app = express();
 
+//Rate Limiting
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 50, // 50 requests,
+  message: 'Too many requests, please try again later.',
+});
+app.use(limiter);
+
 app.use(cors()); //Enable CORS
 app.use(helmet()); //Protects against well known vulnerabilities
-app.use(compression()); //Compress all routes
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

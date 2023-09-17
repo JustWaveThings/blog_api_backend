558b6b4e2ba29836a9b95d64294351d6dc7e9eef
JustWaveThings <justwavethings@proton.me>
2023-09-17 18:25:26 -0400

    Remove duplication, reorder logger

--- app.js
+++ app.js
@@ -60,11 +60,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use(compression()); //Compress all routes
 

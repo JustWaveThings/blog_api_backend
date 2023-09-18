e952577a72676d725f37e9a866fdd473c9a78041
JustWaveThings <justwavethings@proton.me>
2023-09-18 15:20:07 -0400

    Update index route placeholder

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -2,9 +2,5 @@
 import asyncHandler from 'express-async-handler';
 
 export const index = asyncHandler(async (req, res, next) => {
-  res.json({ title: 'homepage Overview' });
-});
-
-export const index2 = asyncHandler(async (req, res, next) => {
-  res.json({ title: 'homepage Overview 2' });
+  res.json({ title: 'Blog API Backend' });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,4 @@ import * as index_controller from '../controllers/index_controller.js';
 
 router.get('/', index_controller.index);
 
-router.get('/2', index_controller.index2);
-
 export default router;

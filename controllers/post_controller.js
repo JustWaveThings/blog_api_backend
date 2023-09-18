4c0e6bb358a900047cdd608c24d62ecd4dc18a8a
JustWaveThings <justwavethings@proton.me>
2023-09-18 15:27:33 -0400

    Create placeholder routing for posts CRUD

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -2,7 +2,31 @@ import Post from '../models/posts';
 import { body, validationResult } from 'express-validator';
 import asyncHandler from 'express-async-handler';
 
-// GET all posts overview
+// get all posts overview (no body, just metadata)
 export const post_list_overview = asyncHandler(async (req, res) => {
   res.json({ title: 'Post List Overview' });
+});
+
+// get single post (with body and comments)
+
+export const post_detail = asyncHandler(async (req, res) => {
+  res.json({ title: 'Post Detail' });
+});
+
+// create post
+
+export const create_post = asyncHandler(async (req, res) => {
+  res.json({ title: 'Create Post' });
+});
+
+// update post
+
+export const update_post = asyncHandler(async (req, res) => {
+  res.json({ title: 'Update Post' });
+});
+
+// delete post
+
+export const delete_post = asyncHandler(async (req, res) => {
+  res.json({ title: 'Delete Post' });
 });
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,23 @@ import express from 'express';
 const router = express.Router();
 import * as post_controller from '../controllers/post_controller';
 
+// get all posts overview (no body, just metadata)
 router.get('/', post_controller.post_list_overview);
+
+// get single post (with body and comments)
+
+router.get('/:id', post_controller.post_detail);
+
+// create post
+
+router.post('/', post_controller.create_post);
+
+// update post
+
+router.put('/:id', post_controller.update_post);
+
+// delete post
+
+router.delete('/:id', post_controller.delete_post);
 
 export default router;

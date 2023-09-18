72a5c95cc88d2ef443843ed45aad1e7dc7d668bc
JustWaveThings <justwavethings@proton.me>
2023-09-18 15:27:12 -0400

    Add comments

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,13 +2,19 @@ import User from '../models/users';
 import { body, validationResult } from 'express-validator';
 import asyncHandler from 'express-async-handler';
 
+// get user
+
 export const get_user = asyncHandler(async (req, res, next) => {
   res.json({ title: 'get user' });
 });
 
+// create user
+
 export const create_user = asyncHandler(async (req, res, next) => {
   res.json({ title: 'create user' });
 });
+
+// update user
 
 export const update_user = asyncHandler(async (req, res, next) => {
   res.json({ title: 'update user' });

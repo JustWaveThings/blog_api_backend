a1fe59b1fe17aecfd596620249b5a9efc04efb6a
JustWaveThings <justwavethings@proton.me>
2023-09-21 16:08:38 -0400

    Create handlers for comments except for update comment

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,14 +1,33 @@
 import Comments from '../models/comments';
+import Post from '../models/posts';
 import { body, validationResult } from 'express-validator';
 import asyncHandler from 'express-async-handler';
 
 // create comment
 
 export const create_comment = asyncHandler(async (req, res) => {
-  res.json({ title: 'Create Comment' });
+  // create comment and save to db
+
+  const comment = await new Comments({
+    name: req.body.name,
+    body: req.body.body,
+    parent_post: req.params.postid,
+    reported: false,
+    published_timestamp: Date.now(),
+  });
+
+  const commentDb = await comment.save();
+
+  // need to add comment to post comment array
+
+  const post = await Post.findById(req.params.postid);
+  post.comment_array.push(comment._id);
+  const postDb = await post.save();
+
+  res.json({ message: 'Comment Successfully Created' });
 });
 
-// update comment
+// update comment // save until I have a comment form... don't forget handling likes
 
 export const update_comment = asyncHandler(async (req, res) => {
   res.json({ title: 'Update Comment' });
@@ -17,11 +36,10 @@ export const update_comment = asyncHandler(async (req, res) => {
 // delete comment (only by admin)
 
 export const delete_comment = asyncHandler(async (req, res) => {
-  res.json({ title: 'Delete Comment' });
-});
-
-// get comments for post
-
-export const get_comments = asyncHandler(async (req, res) => {
-  res.json({ title: 'Get Comments for post' });
+  const comment = await Comments.findByIdAndRemove(req.params.commentid);
+  if (comment && comment._id.toString() === req.params.commentid) {
+    res.json({ message: 'Comment Successfully Deleted.' });
+  } else {
+    res.json({ message: 'There was an error.' });
+  }
 });

a288025ebb9949f714faa0c86848a9a17b7dc0b1
JustWaveThings <justwavethings@proton.me>
2023-09-21 15:04:41 -0400

    All post handlers work except post update(WIP)

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -4,29 +4,59 @@ import asyncHandler from 'express-async-handler';
 
 // get all posts overview (no body, just metadata)
 export const post_list_overview = asyncHandler(async (req, res) => {
-  res.json({ title: 'Post List Overview' });
+  const posts = await Post.find({}, 'title subtitle published created_timestamp comment_count');
+  res.json({ posts });
 });
 
 // get single post (with body and comments)
 
 export const post_detail = asyncHandler(async (req, res) => {
-  res.json({ title: 'Post Detail' });
+  console.log(req.params.postid);
+  const post = await Post.findById(req.params.postid);
+  res.json({ post });
 });
 
 // create post
 
-export const create_post = asyncHandler(async (req, res) => {
-  res.json({ title: 'Create Post' });
-});
+export const create_post = [
+  // validate and sanitize fields
+  body('title', 'Title required').trim().isLength({ min: 1 }).escape(),
+  body('body', 'Body required').trim().isLength({ min: 1 }).escape(),
+  body('subtitle', 'Subtitle required').trim().isLength({ min: 1 }).escape(),
+  body('published').trim().escape(),
 
-// update post
+  asyncHandler(async (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ errors });
+    }
+    const post = new Post({
+      title: req.body.title,
+      body: req.body.body,
+      subtitle: req.body.subtitle,
+      published: req.body.published,
+      created_timestamp: Date.now(),
+    });
+    const db = await post.save();
+    res.json({ message: 'Post Successfully Created' });
+  }),
+];
+
+// update post // save for later.... may wait until I have a form....
 
 export const update_post = asyncHandler(async (req, res) => {
+  // const post = await Post.findById(req.params.postid);
   res.json({ title: 'Update Post' });
 });
 
 // delete post
 
 export const delete_post = asyncHandler(async (req, res) => {
-  res.json({ title: 'Delete Post' });
+  const post = await Post.findByIdAndRemove(req.params.postid);
+  if (post && post._id.toString() === req.params.postid) {
+    res.json({ message: 'Post Successfully Deleted.' });
+  } else {
+    res.json({ message: 'There was an error.' });
+  }
 });

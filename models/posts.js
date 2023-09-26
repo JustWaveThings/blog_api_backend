d050dd937bd130fe511b7c16b84ef850580b1ef7
JustWaveThings <justwavethings@proton.me>
2023-09-26 13:44:39 -0400

    Refactor schema to include toJSON object options directly

--- models/posts.js
+++ models/posts.js
@@ -1,17 +1,20 @@
 import mongoose from 'mongoose';
 import { Schema } from 'mongoose';
 
-const PostSchema = new Schema({
-  title: { type: String, required: true, max: 250 },
-  subtitle: { type: String, required: false, max: 250 },
-  body: { type: String, required: true },
-  published: { type: Boolean, required: true, default: 'false' },
-  created_timestamp: { type: Date, required: true },
-  updated_timestamp: { type: Date, required: false },
-  published_timestamp: { type: Date, required: false },
-  comment_array: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-  likes: { type: Number, required: true, default: 0 },
-});
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true, max: 250 },
+    subtitle: { type: String, required: false, max: 250 },
+    body: { type: String, required: true },
+    published: { type: Boolean, required: true, default: 'false' },
+    created_timestamp: { type: Date, required: true },
+    updated_timestamp: { type: Date, required: false },
+    published_timestamp: { type: Date, required: false },
+    comment_array: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    likes: { type: Number, required: true, default: 0 },
+  },
+  { toJSON: { virtuals: true } }
+);
 
 // virtual for comment count
 PostSchema.virtual('comment_count').get(function () {

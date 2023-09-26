48feb8d2659501d0b2ba2811df0497e0f0c80791
JustWaveThings <justwavethings@proton.me>
2023-09-26 13:43:52 -0400

    Remove comment_array from exclusion so virtual could consume it

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -5,7 +5,7 @@ import asyncHandler from 'express-async-handler';
 // get all posts overview (no body, just metadata)
 export const post_list_overview = asyncHandler(async (req, res) => {
   // exclude posts with the published flag set to false
-  const posts = await Post.find({ published: true }, '-body -comment_array');
+  const posts = await Post.find({ published: true }, '-body ');
   res.json({ posts });
 });
 

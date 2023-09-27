7dde9ecece713c3776d791c0f4a03c613d78a818
JustWaveThings <justwavethings@proton.me>
2023-09-27 16:52:37 -0400

    Refactor post comment controller to send json res per REST principles.

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -7,24 +7,26 @@ import asyncHandler from 'express-async-handler';
 
 export const create_comment = asyncHandler(async (req, res) => {
   // create comment and save to db
+  console.log(req.body);
 
   const comment = await new Comments({
     name: req.body.name,
     body: req.body.body,
-    parent_post: req.params.postid,
+    parent_post: req.params.id,
     reported: false,
     published_timestamp: Date.now(),
   });
 
-  const commentDb = await comment.save();
+  await comment.save();
 
   // need to add comment to post comment array
 
-  const post = await Post.findById(req.params.postid);
+  const post = await Post.findById(req.params.id);
   post.comment_array.push(comment._id);
   const postDb = await post.save();
 
-  res.json({ message: 'Comment Successfully Created' });
+  // res.status(200).redirect(`http:/localhost:3000/posts/${req.params.id}`);
+  res.json({ status: 200 });
 });
 
 // update comment // save until I have a comment form... don't forget handling likes

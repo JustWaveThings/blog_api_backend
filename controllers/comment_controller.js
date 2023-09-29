05373de353c824843a194db9ddcb3ca53f93c880
JustWaveThings <justwavethings@proton.me>
2023-09-29 11:49:28 -0400

    Sanitize comment user input and respond RESTfully

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -5,31 +5,38 @@ import asyncHandler from 'express-async-handler';
 
 // create comment
 
-export const create_comment = asyncHandler(async (req, res) => {
-  // create comment and save to db
-  console.log(req.body);
+export const create_comment = [
+  // validate and sanitize fields
+  body('name', 'Name required').trim().isLength({ min: 1 }).escape(),
+  body('body', 'Body required').trim().isLength({ min: 4 }).escape(),
 
-  const comment = await new Comments({
-    name: req.body.name,
-    body: req.body.body,
-    parent_post: req.params.id,
-    reported: false,
-    published_timestamp: Date.now(),
-  });
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  await comment.save();
+    const comment = await new Comments({
+      name: req.body.name,
+      body: req.body.body,
+      parent_post: req.params.id,
+      reported: false,
+      published_timestamp: Date.now(),
+    });
 
-  // need to add comment to post comment array
+    if (!errors.isEmpty()) {
+      return res.json({ errors });
+    }
 
-  const post = await Post.findById(req.params.id);
-  post.comment_array.push(comment._id);
-  const postDb = await post.save();
+    await comment.save();
 
-  // res.status(200).redirect(`http:/localhost:3000/posts/${req.params.id}`);
-  res.json({ status: 200 });
-});
+    // need to add comment to post comment array
+    const addPost = await Post.findById(req.params.id);
+    addPost.comment_array.push(comment._id);
+    await addPost.save();
 
-// update comment // save until I have a comment form... don't forget handling likes
+    res.json({ status: 200 });
+  }),
+];
+
+// update comment // save until I have a comment form... to handle likes
 
 export const update_comment = asyncHandler(async (req, res) => {
   res.json({ title: 'Update Comment' });

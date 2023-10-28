4a962cce76556ccdced155e90c897d74b697a977
JustWaveThings <justwavethings@proton.me>
2023-10-28 16:27:34 -0400

    chore: remove unused auth middleware functions

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -3,35 +3,8 @@ export const isAuth = (req, res, next) => {
     next();
   } else {
     res.render('error', {
-      message: 'You are not authorized to view this resource because you are not logged in.',
-      error: {
-        status: null,
-        stack: null,
-      },
-    });
-  }
-};
-
-export const isMember = (req, res, next) => {
-  if (req.isAuthenticated() && res.locals.member) {
-    next();
-  } else {
-    res.render('error', {
-      message: 'You are not authorized to view this resource because you are not a member.',
-      error: {
-        status: null,
-        stack: null,
-      },
-    });
-  }
-};
-
-export const isAdmin = (req, res, next) => {
-  if (req.isAuthenticated() && res.locals.admin) {
-    next();
-  } else {
-    res.render('error', {
-      message: 'You are not authorized to view this resource because you are not an Admin.',
+      message:
+        'You are not authorized to view this resource because you are not logged in.',
       error: {
         status: null,
         stack: null,

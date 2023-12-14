ed70c0d21e60a4697520131a1b26785159fd9fed
JustWaveThings <justwavethings@proton.me>
2023-12-14 12:58:14 -0500

    add both FE dev sites to cors origin

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5174', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
